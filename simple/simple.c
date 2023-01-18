e937b8d72e39afa324822315fad813e9e11cbc0b
Austin Chen <austin.yhc@gmail.com>
2023-01-18 21:09:05 +0800

    :bug: Fix the push-pull setting

--- simple/simple.c
+++ simple/simple.c
@@ -5,14 +5,16 @@ asm (".word 0x20001000");
 asm (".word main");
 
 int main() {
+    int c;
     SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOEEN);
     GPIOE->MODER = 0xfffffff7;
-    GPIOE->OTYPER = 0x00000002;
+    GPIOE->OTYPER = 0x00000000;
 
     while(1) {
         GPIOE->BSRR = (1 <<  17);
-        /* for (c = 0; c < 100000; c++); */
+        for (c = 0; c < 100000; c++);
+
         GPIOE->BSRR = (1 <<  1);
-        /* for (c = 0; c < 100000; c++); */
+        for (c = 0; c < 100000; c++);
     }
 }
