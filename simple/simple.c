96872bd15c8f4c5589822e1d58465ee75c0440dd
Austin Chen <austin.yhc@gmail.com>
2023-01-18 20:22:17 +0800

    :alembic: Add a really simple project

---
+++ simple/simple.c
@@ -0,0 +1,18 @@
+#include "stm32l4xx.h"
+#include "stm32l4r9xx.h"
+
+asm (".word 0x20001000");
+asm (".word main");
+
+int main() {
+    SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOEEN);
+    GPIOE->MODER = 0xfffffff7;
+    GPIOE->OTYPER = 0x00000002;
+
+    while(1) {
+        GPIOE->BSRR = (1 <<  17);
+        /* for (c = 0; c < 100000; c++); */
+        GPIOE->BSRR = (1 <<  1);
+        /* for (c = 0; c < 100000; c++); */
+    }
+}
