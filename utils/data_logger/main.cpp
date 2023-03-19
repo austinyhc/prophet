e71808b7984305a38d7411d60c90d2cd58fd6edd
Austin Chen <austin.yhc@gmail.com>
2023-03-20 01:07:12 +0800

    ✨ Add -v option

--- utils/data_logger/main.cpp
+++ utils/data_logger/main.cpp
@@ -26,6 +26,7 @@
 #include "json.hpp"
 #include "cxxopts.hpp"
 
+using std::vector;
 using std::string;
 using std::cout;
 using std::cin;
@@ -74,13 +75,67 @@ void action_load_and_store_device_config() {
     exit(EXIT_SUCCESS);
 }
 
+void action_load_device_descriptor() {
+    if(hs_datalog_open() != ST_HS_DATALOG_OK)
+    {
+        cout << "Error occurred while initializing datalog\n";
+        cout << "Press any key to exit \n";
+        cin.get();
+        exit(EXIT_FAILURE);
+    }
+
+    int n_devices = 0;
+    if(hs_datalog_get_device_number(&n_devices) != ST_HS_DATALOG_OK)
+    {
+        cout << "Error occurred while retrieving device number\n";
+        cout << "Press any key to exit \n";
+        exit(EXIT_FAILURE);
+        getchar();
+    }
+
+    if(n_devices == 0)
+    {
+        cout << "No devices, exiting\n";
+        cout << "Press any key to exit \n";
+        cin.get();
+        exit(EXIT_FAILURE);
+    }
+
+    /* If multiple devices are present, address only device with id = 0 */
+    int device_id = 0;
+    int n_sensors = 0;
+
+    char * tempDev;
+
+    if(hs_datalog_get_device_descriptor(device_id, &tempDev) != ST_HS_DATALOG_OK)
+    {
+        cout << "Error occurred while retrieving device descriptor\n";
+        cout << "Press any key to exit \n";
+        cin.get();
+        exit(EXIT_FAILURE);
+    }
+
+    auto json = nlohmann::json::parse(tempDev);
+    cout << "Device information: \n";
+    cout << json.dump(1) << "\n";
+
+    /* Free memory */
+    if(hs_datalog_free(tempDev) != ST_HS_DATALOG_OK)
+    {
+        cout << "Error occurred while freeing memory\n";
+        cout << "Press any key to exit \n";
+        cin.get();
+        exit(EXIT_FAILURE);
+    }
+}
+
 void action_print_usage(const string& content) {
     cout << content << endl;
     exit(EXIT_SUCCESS);
 }
 
 void action_print_version() {
-    // TODO: What if the HSD Interface cannot be loaded.
+    // TODO : What if the HSD Interface cannot be loaded.
     char * version;
     hs_datalog_get_version(&version);
     cout << "Data Logger CLI 0.0.1" << '\n';
@@ -123,6 +178,104 @@ int main(int argc, char *argv[])
         }
     }
     uint32_t timeout_seconds = result["t"].as<uint32_t>();
+
+    // NOTE: Do we need to read device descriptor before reading number of sensor
+    // NOTE: `deviceID` is set to 0 if multiple devices found
+    // if(hs_datalog_get_sensor_number(deviceID, &nSensors) != ST_HS_DATALOG_OK)
+    // {
+    //     cout << "Error occurred while retrieving sensor number\n";
+    //     cout << "Press any key to exit \n";
+    //     getchar();
+    //     return -1;
+    // }
+
+    // char acq_name[] = "testName";
+    // char acq_description[] = "descriptionTest";
+    // hs_datalog_set_acquisition_param(deviceID, acq_name, acq_description);
+
+
+    vector<vector<bool>> SubsensorIsActive;
+
+    /* Try and open JSON config file on the hard disk*/
+    bool configFromFile = false;
+    if (configFile.is_open())
+    {
+        configFromFile = true;
+        cout << endl <<"Configuration imported from Json file " << endl << endl;
+
+        configFile.seekg (0, configFile.end);
+        long long size = configFile.tellg();
+        configFile.seekg (0, configFile.beg);
+
+        /* Read the whole file */
+        char * jsonChar = new char [size+1];
+        configFile.seekg (0, ios::beg);
+        configFile.read (jsonChar, static_cast<int>(size));
+        configFile.close();
+        jsonChar[size]=0;  // JSON lib needs a '\0' at the end of the string
+
+        /* Parse JSON and extract sensors */
+        auto configJson = nlohmann::json::parse(jsonChar);
+        auto jSensors = configJson.at("device").at("sensor");
+
+        /* Create messages to set sensor status as described in the file and send them to the device*/
+        for (size_t j = 0; j < jSensors.size(); j++ )
+        {
+            auto jSubSensorsStatus = jSensors.at(j).at("sensorStatus").at("subSensorStatus");
+            nlohmann::json cmd;
+            cmd["command"] = "SET";
+            cmd["sensorId"] = jSensors.at(j).at("id");
+
+            SubsensorIsActive.push_back(std::vector<bool>());
+            for(size_t k = 0; k< jSubSensorsStatus.size(); k++ )
+            {
+                nlohmann::json params;
+                auto subId = jSensors.at(j).at("sensorDescriptor").at("subSensorDescriptor").at(k).at("id");
+                params["id"]= subId;
+
+                auto it = jSubSensorsStatus.at(k).begin();
+                auto end = jSubSensorsStatus.at(k).end();
+                for(; it!=end; it++)
+                {
+                    params[it.key()]=it.value();
+                }
+                cmd["subSensorStatus"].push_back(params);
+                SubsensorIsActive[j].push_back(jSensors.at(j).at("sensorStatus").at("subSensorStatus").at(k).at("isActive"));
+            }
+            if(hs_datalog_send_message(deviceID, const_cast <char *>(cmd.dump().data()), static_cast<int>(cmd.dump().length()), nullptr, nullptr) != ST_HS_DATALOG_OK)
+            {
+                cout << "Error occurred while sending message\n";
+                cout << "Press any key to exit \n";
+                getchar();
+                return -1;
+            }
+        }
+        /* Update tag list */
+        auto tag_hw = configJson.at("device").at("tagConfig").at("hwTags");
+        auto tag_sw = configJson.at("device").at("tagConfig").at("swTags");
+
+        auto it= tag_hw.begin();
+        while(it !=tag_hw.end() )
+        {
+            int id=it->at("id");
+            string label=it->at("label");
+            hs_datalog_set_hw_label(deviceID, id, &label[0]);
+
+            bool status =it->at("enabled");
+            hs_datalog_enable_hw_tag(deviceID, id, status);
+            it++;
+        }
+
+        it= tag_sw.begin();
+        while(it !=tag_sw.end() )
+        {
+            int id=it->at("id");
+            string label=it->at("label");
+            hs_datalog_set_sw_label(deviceID, id, &label[0]);
+            it++;
+        }
+        delete[] jsonChar;
+    }
 
     return 0;
 }
